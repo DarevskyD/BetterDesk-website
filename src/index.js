d244e146f53f9707a45d9b5013006d7155c5ec4c
DmitryDarevsky <darevsky.dmitry@gmail.com>
2020-05-04 18:40:10 +0300

    connect scroll.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ window.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
     let sidebar = require('./js/sidebar'),
-        todo = require('./js/todo');
+        todo = require('./js/todo'),
+        scroll = require('./js/scroll');
 
     sidebar();
     todo();
+    scroll();
 
 });
